48dbcab1904083def062a13776b0855416d7c5eb
truptisatsangi <truptisatsangi18@gmail.com>
2026-06-12 17:56:02 +0530

    refactor: made update function further scalable

--- services/cloth.py
+++ services/cloth.py
@@ -1,6 +1,7 @@
 from repository.wardrobe_repository import WardrobeRepository
-from models.cloth import Cloth
+from models import Cloth, ClothUpdate
 from exceptions.cloth_exceptions import ClothNotFoundError
+
 
 class WardrobeService:
     def __init__(self,repository: WardrobeRepository):
@@ -29,15 +30,24 @@ class WardrobeService:
         clothes = self.repository.get_all_clothes()
         return [cloth for cloth in clothes if cloth.ocassion.lower() == occasion.lower()]
     
-    def update_cloth_data(self, cloth_id: str, cloth_name:str, cloth_colour:str,  cloth_occasion:str) -> Cloth:
-        cloth_to_be_updated = self.repository.get_cloth(cloth_id)
+    def update_cloth_data(self, cloth_id: str, update: ClothUpdate) -> Cloth:
+        cloth = self.repository.get_cloth(cloth_id)
 
-        if cloth_to_be_updated:
-            updated_cloth = Cloth(id = cloth_id, name=cloth_name, colour=cloth_colour, occasion=cloth_occasion)
-            self.repository.update_cloth(cloth_id, updated_cloth)
-            return updated_cloth
+        if cloth is None:
+            raise ClothNotFoundError(f"Cloth {cloth_id} not found")
+        
+        if update.name is not None:
+            cloth.name = update.name
+        
+        if update.colour is not None:
+            cloth.colour = update.colour
+            
+        if update.occassion is not None:
+            cloth.occassion = update.occassion
 
-        raise ClothNotFoundError(f"Cloth {cloth_id} not found")
+        self.repository.update_cloth(cloth)
+        return cloth
+            
 
     def remove_cloth(self, cloth_id: str) -> bool:
         cloth = self.repository.get_cloth(cloth_id)
