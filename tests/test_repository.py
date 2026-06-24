8bbb18ef4228f94ec7705197714e061379d7e871
truptisatsangi <truptisatsangi18@gmail.com>
2026-06-24 17:53:31 +0530

    test: added all happy path tests of the repository

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,25 +1,113 @@
-from models import ClothCreate, ClothUpdate, Cloth
+from models import Cloth
 from repository.wardrobe_repository import WardrobeRepository
 
 
 def test_add_cloth():
+    repository = WardrobeRepository()
 
-    warddrobe_repository = WardrobeRepository()
-    test_cloth = Cloth(id="1", name="T-shirt", colour="Black", occasion="Casual")
-    warddrobe_repository.add_cloth(test_cloth)
-    assert warddrobe_repository.clothes["1"] == test_cloth
+    cloth = Cloth(
+        id="1",
+        name="T-shirt",
+        colour="Black",
+        occasion="Casual"
+    )
+
+    repository.add_cloth(cloth)
+
+    assert repository.clothes["1"] == cloth
+
 
 def test_get_cloth():
-    pass
+    repository = WardrobeRepository()
+
+    cloth = Cloth(
+        id="1",
+        name="T-shirt",
+        colour="Black",
+        occasion="Casual"
+    )
+
+    repository.add_cloth(cloth)
+
+    result = repository.get_cloth("1")
+
+    assert result == cloth
+
 
 def test_get_cloth_returns_none_when_not_found():
-    pass
+    repository = WardrobeRepository()
+
+    result = repository.get_cloth("999")
+
+    assert result is None
+
 
 def test_get_all_clothes():
-    pass
+    repository = WardrobeRepository()
+
+    cloth1 = Cloth(
+        id="1",
+        name="T-shirt",
+        colour="Black",
+        occasion="Casual"
+    )
+
+    cloth2 = Cloth(
+        id="2",
+        name="Shirt",
+        colour="Blue",
+        occasion="Office"
+    )
+
+    repository.add_cloth(cloth1)
+    repository.add_cloth(cloth2)
+
+    result = repository.get_all_clothes()
+
+    assert len(result) == 2
+    assert cloth1 in result
+    assert cloth2 in result
+
 
 def test_update_cloth():
-    pass
+    repository = WardrobeRepository()
+
+    cloth = Cloth(
+        id="1",
+        name="T-shirt",
+        colour="Black",
+        occasion="Casual"
+    )
+
+    repository.add_cloth(cloth)
+
+    updated_cloth = Cloth(
+        id="1",
+        name="Formal Shirt",
+        colour="White",
+        occasion="Office"
+    )
+
+    repository.update_cloth("1", updated_cloth)
+
+    result = repository.get_cloth("1")
+
+    assert result == updated_cloth
+    assert result.name == "Formal Shirt"
+
 
 def test_delete_cloth():
-    pass
+    repository = WardrobeRepository()
+
+    cloth = Cloth(
+        id="1",
+        name="T-shirt",
+        colour="Black",
+        occasion="Casual"
+    )
+
+    repository.add_cloth(cloth)
+
+    repository.delete_cloth("1")
+
+    assert repository.get_cloth("1") is None
